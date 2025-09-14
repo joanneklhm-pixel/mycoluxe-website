cc4c9c851ac92121aa4562abb46d88980cfb5485
joanne Kelham <joanneklhm@gmail.com>
2025-09-14 08:40:26 +0200

    Fix repo structure: moved src and config files to correct level

--- Mycoluxe-website/src/src/components/ContactSupportPage.jsx
+++
@@ -1,7 +0,0 @@
-export default function ContactSupportPage() {
-  return (
-    <div className="bg-black text-white min-h-screen flex items-center justify-center">
-      <h1 className="text-gold text-4xl font-serif">ContactSupportPage Page</h1>
-    </div>
-  );
-}
--- Mycoluxe-website/src/src/components/EducationHub.jsx
+++
@@ -1,7 +0,0 @@
-export default function EducationHub() {
-  return (
-    <div className="bg-black text-white min-h-screen flex items-center justify-center">
-      <h1 className="text-gold text-4xl font-serif">EducationHub Page</h1>
-    </div>
-  );
-}
